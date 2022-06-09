eb6550c3e1aea0838e292e4c8e6c58a7d7212292
Kevin Primm <kfprimm@gmail.com>
2022-06-08 23:17:05 -0400

    Added ubuntu

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 IMAGE=ghcr.io/blitz3d-ng/env
 
-for platform in android debian-10 debian-11 nx mingw emscripten
+for platform in android debian-10 debian-11 ubuntu-21.04 ubuntu-22.04 nx mingw emscripten
 do
-  docker build -t $IMAGE:$platform - < Dockerfile.$platform
+  docker build -t $IMAGE:$platform --build-arg RUBY_VERSION=3.1.2 --build-arg RUBY_INSTALL_VERSION=0.8.3 - < Dockerfile.$platform
 done
