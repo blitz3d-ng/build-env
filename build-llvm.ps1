f125714b59e6ac9264060dc6f5020023243e4552
Kevin Primm <kfprimm@gmail.com>
2023-03-05 15:38:46 -0500

    win test

--- build-llvm.ps1
+++ build-llvm.ps1
@@ -1,19 +1,16 @@
-$LLVM_VERSION = "14.0.4"
+$LLVM_VERSION = "16.0.0-rc3"
 $BUILD_DIR = ".\build"
 $DEST_DIR = ".\llvm"
 
 Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-$LLVM_VERSION.zip" -OutFile "llvm.zip"
 Expand-Archive llvm.zip -DestinationPath .\
 
-cmake -S llvm-project-llvmorg-$LLVM_VERSION\llvm -B $BUILD_DIR `
-  -DCMAKE_BUILD_TYPE=Release `
-  -DCMAKE_INSTALL_PREFIX=$DEST_DIR `
-  -DCMAKE_CXX_STANDARD=14 `
-  -DLLVM_ENABLE_PROJECTS=lld `
-  -DLLVM_ENABLE_RUNTIMES="" `
-  -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;WebAssembly;X86" `
-  -DLLVM_BUILD_TOOLS=OFF `
-  -DLLVM_HAVE_LIBXAR=OFF
+cmake `
+  -G Ninja `
+  -S llvm-project-llvmorg-$LLVM_VERSION\llvm `
+  -B $BUILD_DIR `
+  -DCMAKE_TOOLCHAIN_FILE=llvm.cmake `
+  -DCMAKE_INSTALL_PREFIX=$DEST_DIR
 
 cmake --build $BUILD_DIR
 cmake --install $BUILD_DIR
