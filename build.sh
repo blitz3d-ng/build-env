515298534e22fd258c93a7df7146e8604b939936
Kevin Primm <kfprimm@gmail.com>
2023-03-06 23:06:28 -0500

    LLVM for win64 + macos

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 set -e
 
 IMAGE=ghcr.io/blitz3d-ng/env
+PLATFORMS=${1:-"debian-10 debian-11 ubuntu-20.04 ubuntu-22.04 android nx mingw emscripten"}
 
-for platform in debian-10 debian-11 ubuntu-20.04 ubuntu-22.04 android nx mingw emscripten
+echo "Building $PLATFORMS..."
+
+for platform in $PLATFORMS
 do
   docker build -t $IMAGE:$platform -f Dockerfile.$platform .
 done
