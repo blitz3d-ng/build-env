c586e4fde96051f1857c29638df6fc279a14ac7f
Kevin Primm <kfprimm@gmail.com>
2023-11-26 13:38:57 -0500

    Added tag to build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ set -e
 
 IMAGE=ghcr.io/blitz3d-ng/env
 PLATFORMS=${1:-"debian-10 debian-11 ubuntu-20.04 ubuntu-22.04 archlinux android nx mingw emscripten"}
+VERSION=v8
 
 echo "Building $PLATFORMS..."
 
 for platform in $PLATFORMS
 do
-  docker build -t $IMAGE:$platform -f Dockerfile.$platform .
+  docker build -t $IMAGE:$platform-$VERSION -f Dockerfile.$platform .
 done
