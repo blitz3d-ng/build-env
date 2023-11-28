d77cf217ed046b431785f2ce723a6b9900017982
Kevin Primm <kfprimm@gmail.com>
2023-11-28 16:18:07 -0500

    [v9] Added DEVKITPRO ENV to Dockerfile.nx

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 IMAGE=ghcr.io/blitz3d-ng/env
 PLATFORMS=${1:-"debian-10 debian-11 ubuntu-20.04 ubuntu-22.04 archlinux android nx mingw emscripten"}
-VERSION=v8
+VERSION=v9
 
 echo "Building $PLATFORMS..."
 
