9344af5cb50c8996ca27a6f8d35074ae01ac5199
Kevin Primm <kfprimm@gmail.com>
2024-12-18 16:44:38 -0500

    Updated android environment

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -e
 
 IMAGE=ghcr.io/blitz3d-ng/env
 PLATFORMS=${1:-"debian-10 debian-11 debian-12 ubuntu-20.04 ubuntu-22.04 ubuntu-24.04 archlinux android nx mingw emscripten"}
-VERSION=v11
+VERSION=v20241218
 
 echo "Building $PLATFORMS..."
 
 for platform in $PLATFORMS
 do
-  docker build -t $IMAGE:$platform-$VERSION -f Dockerfile.$platform .
+  docker build --platform=linux/x86_64 -t $IMAGE:$platform-$VERSION -f Dockerfile.$platform .
 done
